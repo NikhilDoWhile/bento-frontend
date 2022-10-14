68652d9732339d3e628db553d79f7047a8a66dee
Nikhil <nikhil4.deswal@gmail.com>
2022-10-14 05:41:51 -0700

    app updated

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native'
+import TextInputComponent from './src/components/TextInputComponent'
+import { StatusBar } from 'expo-status-bar'
+import LoginScreen from './src/screen/LoginScreen'
+import StackNavigators from './src/navigation/StackNavigators'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={styles.container}>
 
+      {/* <LoginScreen /> */}
+      <StackNavigators />
+    </SafeAreaView>
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+    //backgroundColor: 'red'
+  }
+
+})
+export default App

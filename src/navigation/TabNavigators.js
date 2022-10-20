f608f7070455ceeaa13a697edc0c7ebe098b24f3
Saurabh <saurabhsindhu96@gmail.com>
2022-10-19 22:16:25 -0700

    Screen navigation
    Stack and Tab navigation

--- src/navigation/TabNavigators.js
+++ src/navigation/TabNavigators.js
@@ -3,6 +3,10 @@ import { Image } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from '../screen/HomeScreen';
+import LunchBoxScreen from '../screen/LunchBoxScreen';
+import ShoppingListScreen from '../screen/ShoppingList';
+import RatingScreen from '../screen/RatingScreen';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 
 const Tab = createBottomTabNavigator();
@@ -11,12 +15,50 @@ const TabNavigators = () => {
         <Tab.Navigator
             initialRouteName='Home'
             screenOptions={{ headerShown: false }}
-            tabBarOptions={{
-                activeTintColor: '#F79489',
-                inactiveTintColor: 'black',
-            }}
+        // tabBarOptions={{
+        //     activeTintColor: 'gray',
+        //     inactiveTintColor: 'black',
+        // }}
         >
-            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({ tintColor, focused }) => (
+                        <AntDesign size={20} color="black" name="home" />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="LunchBox"
+                component={LunchBoxScreen}
+                options={{
+                    tabBarIcon: ({ tintColor, focused }) => (
+                        <AntDesign size={20} color="black" name="appstore-o" />
+                    )
+                }}
+
+            />
+            <Tab.Screen
+                name="ShoppingList"
+                component={ShoppingListScreen}
+                options={{
+                    tabBarIcon: ({ tintColor, focused }) => (
+                        <AntDesign size={20} color="black" name="shoppingcart" />
+                    )
+                }}
+
+            />
+            <Tab.Screen
+                name="Rating"
+                component={RatingScreen}
+                options={{
+                    tabBarIcon: ({ tintColor, focused }) => (
+                        <AntDesign size={20} color="black" name="staro" />
+                    )
+                }}
+
+            />
 
         </Tab.Navigator>
     );

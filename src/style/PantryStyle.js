1bc4df09680c0d801534997a6cb4e9e6fcb6de20
Saurabh <saurabhsindhu96@gmail.com>
2022-10-19 22:23:53 -0700

    Create PantryStyle.js
    Styling of pantry

---
+++ src/style/PantryStyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const pantryStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    mainContainer: {
+        // flex: 9 / 10,
+        alignItems: 'center'
+    },
+    itemContainer: {
+        height: 90,
+        width: 90,
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    ingridientsView: {
+        height: 60,
+        width: 60,
+    }
+
+})
+

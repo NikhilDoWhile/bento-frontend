525c524d295fb5812586391c0723cec5524e1d33
Rahul Naranthatta <rnaranthatta00@mylangara.ca>
2022-10-25 20:52:36 -0700

    Updated LunchBox Screen.js

--- src/screen/LunchBoxScreen.js
+++ src/screen/LunchBoxScreen.js
@@ -1,23 +1,50 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, Text, FlatList, Dimensions, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from '../components/Header'
+const screenHeight = Dimensions.get('window').height
+const screenWidth = Dimensions.get('window').width
+import Feather from 'react-native-vector-icons/Feather';
+import LunchBoxComponent from '../components/LunchBoxComponent'
 
-const LunchBoxScreen = () => {
-
+const LunchBoxScreen = ({ route, navigation }) => {
+    console.log("oute?.params?.message===", route?.params)
+    const [data, setData] = useState(route?.params?.message)
+    const [flag, setFlag] = useState(false)
+    useEffect(() => {
+        setFlag(route?.params?.flag)
+    }, [])
+    const renderItem = ({ item }) => {
+        { setData(route?.params?.message) }
+        return (
+            /* Styling insert */
+            <Text>{item.day}</Text>
+        )
+    }
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={{ fontSize: 30, alignSelf: 'center' }}>LunchBox</Text>
-            <View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+            <Header
+                showHeaderTitle
+                headerTitle="Lunch Box"
+            />
+            {
+                flag ?
+                    <FlatList
+                        data={data}
+                        keyExtractor={(item) => item.id}
+                        renderItem={renderItem}
+                    /> :
+                    <LunchBoxComponent
+                        content={'Find a Recipe and assign to a lunch box for it to appear here'}
+                        onRecipePress={() => navigation.navigate('Home')}
+                    />
+            }
 
-            </View>
+
         </SafeAreaView>
     )
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'orange'
-    }
 
 })
 export default LunchBoxScreen

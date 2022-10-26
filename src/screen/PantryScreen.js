751678c2d5f1a497d6d4bd88185d392f26e69635
Saurabh <saurabhsindhu96@gmail.com>
2022-10-25 21:01:28 -0700

    Screens Styling
    Pantry and Welcome screens styling updated

--- src/screen/PantryScreen.js
+++ src/screen/PantryScreen.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import React, { useState } from 'react'
-import { StyleSheet, View, Text, ScrollView, FlatList, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, ScrollView, FlatList, TouchableOpacity, Dimensions } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import ButtonComponent from '../components/ButtomComponent'
 import Header from '../components/Header'
@@ -8,8 +8,8 @@ import ImageComponent from '../components/ImageComponent'
 import SearchComponent from '../components/SearchComponent'
 import { pantryStyle } from '../style/PantryStyle'
 
+
 const PantryScreen = ({ navigation }) => {
-    // add data for ingredients
     let Data = [
         { id: 1, ingridientsName: 'pasta', ingridientsImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4pJecJIYHKmK5gBbCJGMTPcTYzNDvgnDkP-c7nWvz&s' },
         { id: 2, ingridientsName: 'Egg', ingridientsImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqdCOGOnJJowcXiFm0pLRtHhuRd8cj3cpqXAf2NE0YzKUZLoSvdPJQBy6MB3A64QooNPw&usqp=CAU' },
@@ -26,8 +26,16 @@ const PantryScreen = ({ navigation }) => {
         { id: 13, ingridientsName: 'papper', ingridientsImage: 'https://thumbs.dreamstime.com/b/black-pepper-macro-soft-focus-39835307.jpg' },
         { id: 14, ingridientsName: 'mayo', ingridientsImage: 'https://hips.hearstapps.com/delish/assets/15/20/1431639912-delish-mayo-jar.jpg' },
         { id: 15, ingridientsName: 'ketchup', ingridientsImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtzfxORbVOEtkPH87mAt2Ixj1WtsH8sRXNdbs55pKO&s' },
+        { id: 9, ingridientsName: 'chicken', ingridientsImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRME_za8F_WIgZnIpiCGlJu6yEpmIZwMmgf2Q&usqp=CAU' },
+        { id: 10, ingridientsName: 'cheese', ingridientsImage: 'https://media.istockphoto.com/photos/various-types-of-cheese-picture-id629210154?k=20&m=629210154&s=612x612&w=0&h=1J-MM9ba8evv1xZL6KxKipmdP-NzXqXWBILsUqs3JIo=' },
+        { id: 11, ingridientsName: 'floor', ingridientsImage: 'https://miro.medium.com/max/1000/1*E1XbXpyexi8eCyH22vUPdw.jpeg' },
+        { id: 12, ingridientsName: 'milk', ingridientsImage: 'https://thumbs.dreamstime.com/b/milk-splash-white-cup-33967774.jpg' },
+        { id: 13, ingridientsName: 'papper', ingridientsImage: 'https://thumbs.dreamstime.com/b/black-pepper-macro-soft-focus-39835307.jpg' },
+        { id: 14, ingridientsName: 'mayo', ingridientsImage: 'https://hips.hearstapps.com/delish/assets/15/20/1431639912-delish-mayo-jar.jpg' },
+        { id: 15, ingridientsName: 'ketchup', ingridientsImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtzfxORbVOEtkPH87mAt2Ixj1WtsH8sRXNdbs55pKO&s' },
     ]
     const [list, setList] = useState(Data)
+    const [filterData, setFilterData] = useState(Data)
 
     const renderItem = ({ item }) => {
         return (
@@ -42,32 +50,51 @@ const PantryScreen = ({ navigation }) => {
             </TouchableOpacity>
         )
     }
+    const searchFilter = (text) => {
+        if (text) {
+            // setCheckvalidation(false)
+            const newData = list.filter((item) => {
+                const itemData = item.ingridientsName ? item.ingridientsName.toUpperCase() : ''.toUpperCase();
+                const textData = text.toUpperCase();
+                return itemData.indexOf(textData) > -1
+            })
+            setFilterData(newData);
+            // setSearch('')
+        } else {
+            setFilterData(list)
+            // setSearch('')
+            // setCheckvalidation(true)
+        }
+
+    }
 
     return (
         <SafeAreaView style={[pantryStyle.container]}>
-            <ScrollView>
-                <Header />
-                <View style={[pantryStyle.mainContainer]}>
-                    <Text style={{ fontSize: 17, marginTop: 10 }}>Please Select the ingridients you have available</Text>
-                    <SearchComponent />
-                    <FlatList
-                        data={list}
-                        keyExtractor={(item) => item.id}
-                        numColumns={3}
-                        renderItem={renderItem}
-                    />
+            {/* <ScrollView> */}
+            <Header />
+            <View style={[pantryStyle.mainContainer]}>
+                <Text style={{ fontSize: 17, marginTop: 10 }}>Please Select the ingridients you have available</Text>
+                <SearchComponent
+                    onChangeText={(text) => searchFilter(text)}
+                />
+                <FlatList
+                    data={filterData}
+                    keyExtractor={(item) => item.id}
+                    numColumns={3}
+                    renderItem={renderItem}
+                />
 
-                </View>
-                <View style={{ flex: 1 / 10 }}>
-                    <ButtonComponent
-                        text={"Proceed"}
-                        buttonStyle={{ borderRadius: 20 }}
-                        onPress={() => navigation.navigate('TabNavigators', {
-                            screen: 'Home'
-                        })}
-                    />
-                </View>
-            </ScrollView>
+            </View>
+            <View style={{ flex: 1 / 10 }}>
+                <ButtonComponent
+                    text={"Proceed"}
+                    buttonStyle={{ borderRadius: 20 }}
+                    onPress={() => navigation.navigate('TabNavigators', {
+                        screen: 'Home'
+                    })}
+                />
+            </View>
+            {/* </ScrollView> */}
         </SafeAreaView>
     )
 }
--- src/screen/WelcomeScreen.js
+++ src/screen/WelcomeScreen.js
@@ -1,40 +1,43 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Dimensions, ScrollView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import ImageComponent from '../components/ImageComponent'
 import ButtonComponent from '../components/ButtomComponent'
 import { welcomeStyle } from '../style/WelcomeStyle'
+const screenHeight = Dimensions.get('window').height
+const screenWidth = Dimensions.get('window').width
 
 const WelcomeScreen = ({ navigation }) => {
 
     return (
         <SafeAreaView style={[welcomeStyle.container]}>
-            <StatusBar style='auto' />
-            <View style={[welcomeStyle.ImageView]}>
-                <ImageComponent
-                    source={'https://cdn5.vectorstock.com/i/1000x1000/48/74/hand-drawn-of-welcome-logo-vector-23994874.jpg'}
-                    imageStyle={{ width: 150 }}
-                />
-            </View>
-            <View style={[welcomeStyle.welcomeView]}>
-                <View style={[welcomeStyle.textView]}>
-                    <View style={{ flex: 0.9, justifyContent: 'space-evenly', margin: 10 }}>
-                        <Text style={{ color: 'black', fontSize: 25, alignSelf: 'center' }}>Welcome UserName!</Text>
-                        <Text style={{ fontSize: 16 }}>Would you like to know what is your pantry based on which you can suggest you the recipes</Text>
-                        <View style={[welcomeStyle.forwardArrowView]}>
+            <ScrollView>
+                <StatusBar style='auto' />
+                <View style={[welcomeStyle.welcomeView]}>
+                    <View style={[welcomeStyle.textView]}>
+                        <ImageComponent
+                            source={'https://img.freepik.com/free-photo/flat-lay-batch-cooking-composition_23-2148765597.jpg?w=2000'}
+                            imageStyle={{ width: screenWidth, height: screenHeight / 2, borderRadius: 20 }}
+                        />
+                        <View style={{ height: screenHeight / 2, justifyContent: 'space-evenly', alignItems: 'center' }}>
+                            <Text style={{ color: '#FFAB00', fontSize: 25, alignSelf: 'center' }}>Welcome UserName!</Text>
+                            <Text style={{ fontSize: 16 }}>Would you like to know what is your pantry based on which you can suggest you the recipes</Text>
+                            <Text style={{ fontSize: 16 }}>Also let us know about your kids</Text>
+                            {/* <View style={[welcomeStyle.forwardArrowView]}>
                             <ImageComponent
                                 source={'https://previews.123rf.com/images/tanyastock/tanyastock1803/tanyastock180300490/97923644-user-icon-avatar-login-sign-circle-button-with-soft-color-gradient-background-vector-.jpg'}
                             />
+                        </View> */}
+                            <ButtonComponent
+                                buttonStyle={[welcomeStyle.welcomeButtonStyle]}
+                                onPress={() => navigation.navigate('Pantry')}
+                                showArrow
+                            />
                         </View>
-                        <ButtonComponent
-                            buttonStyle={[welcomeStyle.welcomeButtonStyle]}
-                            onPress={() => navigation.navigate('Pantry')}
-                            showArrow
-                        />
                     </View>
                 </View>
-            </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }

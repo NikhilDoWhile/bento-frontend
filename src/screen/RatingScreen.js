ffd5327eb3641450b29ce64e1cabae944711c885
Saurabh <saurabhsindhu96@gmail.com>
2022-10-28 01:48:07 -0700

    Shopping list and Rating section
    Static page development of shopping list and ratings.

--- src/screen/RatingScreen.js
+++ src/screen/RatingScreen.js
@@ -1,22 +1,65 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, FlatList, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from '../components/Header'
 
-const RatingScreen = () => {
+const RatingScreen = ({ navigation }) => {
+    const List = [
+        { id: 1, name: "bella", rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+        { id: 2, name: 'jeena', rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+        { id: 3, name: "bella", rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+        { id: 4, name: 'jeena', rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+        { id: 5, name: "bella", rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+        { id: 6, name: 'jeena', rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+        { id: 7, name: "bella", rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+        { id: 8, name: 'jeena', rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+        { id: 9, name: "bella", rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+        { id: 10, name: 'jeena', rate: 'https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Blushed-Smiling-Emoji-Free-Download-IOS-Emojis.png', date: '15/12/2022', recipe: 'Ham Sandwich' },
+    ]
+    const [rating, setRating] = useState(List)
+
+    const renderItem = ({ item }) => {
+        return (
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10, marginTop: 10 }}>
+                <Text style={{ fontSize: 14, }}>{item.date}</Text>
+                <Text style={{ fontSize: 14, right: 15 }} >{item.name}</Text>
+                <Text style={{ fontSize: 14, right: 10 }}>{item.recipe}</Text>
+                <Image
+                    source={{ uri: item.rate }}
+                    style={{ height: 20, width: 20, right: 10 }}
+                />
+            </View>
+        )
+    }
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={{ fontSize: 30, alignSelf: 'center' }}>Rating</Text>
-            <View>
-
+            <Header
+                showHeaderTitle
+                headerTitle={'Rating'}
+                onBackPress={() => navigation.pop()}
+            />
+            <View style={{}}>
+                <Text style={{ fontSize: 25, fontWeight: 'bold', margin: 10 }}>Rating</Text>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 10 }}>
+                    <Text style={{ fontSize: 18, fontWeight: '800' }}>Date</Text>
+                    <Text style={{ fontSize: 18, fontWeight: '800' }} >Name</Text>
+                    <Text style={{ fontSize: 18, fontWeight: '800' }}>Recipe</Text>
+                    <Text style={{ fontSize: 18, fontWeight: '800' }} >Rating</Text>
+                </View>
             </View>
+            <FlatList
+                data={List}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
+            />
         </SafeAreaView>
     )
 }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'green'
+        //backgroundColor: 'green'
     }
 
 })
--- src/screen/ShoppingList.js
+++ src/screen/ShoppingList.js
@@ -1,14 +1,62 @@
-import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, View, Text, FlatList } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import ButtonComponent from '../components/ButtomComponent'
+import Header from '../components/Header'
+import SearchComponent from '../components/SearchComponent'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Entypo from 'react-native-vector-icons/Entypo';
 
 const ShoppingListScreen = () => {
+    const List = [
+        { id: 1, data: 'Wheat Totilla' },
+        { id: 2, data: 'Egg Salad Sandwitch' },
+        { id: 3, data: 'Noodles' },
+        { id: 4, data: 'Momos' },
+        { id: 5, data: 'Pancakes' },
+        { id: 6, data: 'Wrap Chicken' },
+        { id: 7, data: 'Potato Rolls' }
+    ]
+    const [shoppingData, setShoppingData] = useState(List)
+    const [select, setSelect] = useState(true)
+    const renderItem = ({ item }) => {
+        return (
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 20, marginTop: 10 }}>
+                <View style={{ flexDirection: 'row' }}>
+                    <MaterialCommunityIcons color={'black'} size={25} name={select ? "checkbox-blank-outline" : 'checkbox-marked-outline'} />
+                    <Text style={{ fontSize: 16, left: 10 }}>{item.data}</Text>
+                </View>
+                <View style={{ height: 20, width: 20, backgroundColor: 'white', borderRadius: 20, justifyContent: 'center', alignItems: 'center' }}>
+                    <Entypo color={'black'} size={20} name='cross' />
+                </View>
+
+            </View>
+        )
+    }
 
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={{ fontSize: 30, alignSelf: 'center' }}>Shopping List</Text>
-            <View>
-
+            <Header
+                showHeaderTitle
+                headerTitle={'Shopping List'}
+            />
+            <View style={{ flex: 1, }}>
+                <View style={{ flex: 9 / 10, }}>
+                    <SearchComponent
+                        searchStyle={{ borderColor: 'gray' }}
+                    />
+                    <FlatList
+                        data={shoppingData}
+                        keyExtractor={(item) => item.id}
+                        renderItem={renderItem}
+                    />
+                </View>
+                <View style={{ flex: 1 / 10, }}>
+                    <ButtonComponent
+                        buttonStyle={{ width: "40%", alignSelf: 'center', borderRadius: 20 }}
+                        text={'Done'}
+                    />
+                </View>
             </View>
         </SafeAreaView>
     )

08d8849c32119837b0862116bfd1a84bb2b225cb
Rahul Naranthatta <rnaranthatta00@mylangara.ca>
2022-11-08 23:20:10 -0800

    lunch box screen

--- src/screen/LunchBoxScreen.js
+++ src/screen/LunchBoxScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, Text, FlatList, Dimensions, Image, Modal, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, FlatList, Dimensions, Image, Modal, TouchableOpacity, ImageBackground } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import Header from '../components/Header'
 const screenHeight = Dimensions.get('window').height
@@ -8,38 +8,75 @@ import Feather from 'react-native-vector-icons/Feather';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import LunchBoxComponent from '../components/LunchBoxComponent'
 import ButtonComponent from '../components/ButtomComponent'
+import Entypo from 'react-native-vector-icons/Entypo';
+import ApiHandler from '../api/ApiHandler'
 
 const LunchBoxScreen = ({ route, navigation }) => {
+    // console.log("lunchBox=====",route.params)
     const list = [
-        { id: 1, name: 'bella' },
-        { id: 2, name: 'jenna' }
+        { id: 1, name: 'bella', checked: 'false' },
+        { id: 2, name: 'jenna', clecked: 'false' }
     ]
-    const [data, setData] = useState(route?.params?.message)
+    const [data, setData] = useState([])
     const [flag, setFlag] = useState(false)
     const [modalVisible, setModalVisible] = useState(false);
     const [select, setSelect] = useState(true)
+    const [nameData, setnameData] = useState(list)
     useEffect(() => {
         setFlag(route?.params?.flag)
+        getLunchBox()
     }, [])
     const modalPopUp = () => {
         setModalVisible(!modalVisible)
     }
-    const showButton = () => {
-        setSelect(!select)
+    const getLunchBox = ()=>{
+        ApiHandler.getLunchBox(route?.params?.parentId).then((response)=>{
+           if(response) {
+              setData(response.day.day)
+           }
+        })
     }
-    const renderItem = ({ item }) => {
-        { setData(route?.params?.message) }
+    const showButton = (value, index) => {
+        const newValue = nameData.map((checkbox, i) => {
+            if (i !== index)
+                return {
+                    ...checkbox,
+                    checked: false,
+                }
+            if (i === index) {
+                const item = {
+                    ...checkbox,
+                    checked: !checkbox.checked,
+                }
+                return item
+            }
+            return checkbox
+        })
+        setnameData(newValue)
+    }
+    const removeData = (id) => {
+        let newArray = [...data]
+        newArray.splice(id, 1)
+        setData(newArray)
+    }
+    const renderItem = ({ item,index }) => {
         return (
             <TouchableOpacity onPress={() => modalPopUp()} style={{ width: '100%', height: screenHeight / 3.5, backgroundColor: 'white', marginTop: 10, justifyContent: 'space-evenly', }}>
-                <Text style={{ fontSize: 20, marginLeft: 22 }}>{item.day}</Text>
+                <Text style={{ fontSize: 20, marginLeft: 22 }}>{item}</Text>
                 <View style={{ width: screenWidth / 1.14, height: screenHeight / 4.5, backgroundColor: '#F6F3E7', alignSelf: 'center', borderRadius: 10 }}>
-                    <Image
-                        source={{ uri: item.recipeImage }}
+                    <ImageBackground
+                        source={{ uri: route.params.recipeImage }}
                         style={{ flex: 7 / 8, borderTopLeftRadius: 10, borderTopRightRadius: 10 }}
-                    />
+                    >
+                        <TouchableOpacity onPress={() => removeData(index)} style={{ backgroundColor: 'white', width: 40, alignSelf: 'flex-end', justifyContent: 'center', alignItems: 'center' }}>
+                            <Entypo size={29} color="black" name="cross" />
+                        </TouchableOpacity>
+                    </ImageBackground>
                     <View style={{ flexDirection: 'row', justifyContent: 'space-between', flex: 2 / 8, alignItems: 'center', paddingHorizontal: 10 }}>
-                        <Text>{item.recipeName}</Text>
+                        <Text>{route.params.ingridientsName}</Text>
+                        <View>
                         <Feather size={22} color="gray" name="user" />
+                        </View>
                     </View>
                 </View>
                 <Modal
@@ -54,16 +91,16 @@ const LunchBoxScreen = ({ route, navigation }) => {
                         </View>
                         <View>
                             <FlatList
-                                data={list}
+                                data={nameData}
                                 keyExtractor={(item) => item.id}
-                                renderItem={({ item }) => {
+                                renderItem={({ item, index }) => {
                                     return (
                                         <View style={{ flexDirection: 'row', justifyContent: 'space-around', }}>
                                             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                                                 <Feather size={22} color="gray" name="user" />
                                                 <Text style={{ marginLeft: 10 }}>{item.name}</Text>
                                             </View>
-                                            <MaterialCommunityIcons color={'#FC6474'} onPress={() => showButton()} size={25} name={select ? "checkbox-blank-circle-outline" : 'checkbox-marked-circle-outline'} />
+                                            <MaterialCommunityIcons color={'#FC6474'} onPress={() => showButton(item.checked, index)} size={25} name={item.checked ? "checkbox-blank-circle-outline" : 'checkbox-marked-circle-outline'} />
                                         </View>
                                     )
                                 }}
@@ -87,17 +124,18 @@ const LunchBoxScreen = ({ route, navigation }) => {
         )
     }
     return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: 'white', opacity: modalVisible === true ? 0.1 : 1, justifyContent: 'center', }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'white', opacity: modalVisible === true ? 0.1 : 1, }}>
             <Header
                 showHeaderTitle
                 headerTitle="Lunch Box"
+                onUserPress={()=>navigation.navigate('ProfileScreen')}
             />
             {
                 flag ?
                     <View>
                         <FlatList
                             data={data}
-                            keyExtractor={(item) => item.id}
+                           // keyExtractor={(item) => item.id}
                             renderItem={renderItem}
                         />
                     </View>

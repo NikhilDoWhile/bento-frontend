1ae237221750d7cad7aef7adb0182f40ef064372
Rahul Naranthatta <rnaranthatta00@mylangara.ca>
2022-11-23 00:26:56 -0800

    Bug fixes in components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,79 @@
-import React from 'react'
-import { StyleSheet, View, Text,Image } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import Feather from 'react-native-vector-icons/Feather';
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import Feather from "react-native-vector-icons/Feather";
 
-const Header = ({ logo, onBackPress, headerTitle, showHeaderTitle,onUserPress }) => {
+const Header = ({
+  logo,
+  onBackPress,
+  headerTitle,
+  showHeaderTitle,
+  onUserPress,
+  onNotiPress,
+}) => {
+  return (
+    <View style={styles.container}>
+      {logo ? (
+        <Image
+          source={require("../../assets/bentoLogo-grn.png")}
+          style={{ height: 18, width: 97 }}
+        />
+      ) : (
+        <AntDesign
+          onPress={onBackPress}
+          size={20}
+          color="gray"
+          name="arrowleft"
+        />
+      )}
+      {showHeaderTitle ? (
+        <Text style={{ fontSize: 17, fontWeight: "900" }}>{headerTitle}</Text>
+      ) : null}
 
-    return (
-        <View style={styles.container}>
-            {
-                logo ? 
+      <View
+        style={{
+          flexDirection: "row",
+          width: 50,
+          justifyContent: "space-between",
+          marginRight: 7
+        }}
+      >
+        {/* <Feather onPress={onUserPress} size={22} color="gray" name="user" />
+                <AntDesign  onPress={onNotiPress} size={20} color="gray" name="bells" /> */}
+        <TouchableWithoutFeedback onPress={onUserPress}>
+          <Image
+            source={require("../../assets/user_Profile.png")}
+            style={{ height: 22, width: 22 }}
+            resizeMode="contain"
+          />
+        </TouchableWithoutFeedback>
 
-                <Image
-                source={require('../../assets/bentoLogo-grn.png')}
-                style={{height:18,width:97}}
-            />
-                
-                : <AntDesign onPress={onBackPress} size={20} color="gray" name="arrowleft" />
-            }
-            {
-                showHeaderTitle ? <Text style={{ fontSize: 17, fontWeight: '900' }}>{headerTitle}</Text> : null
-            }
-
-
-            <View style={{ flexDirection: 'row', width: 50, justifyContent: 'space-between' }}>
-                <Feather onPress={onUserPress} size={22} color="gray" name="user" />
-                <AntDesign size={20} color="gray" name="bells" />
-            </View>
-        </View>
-    )
-}
+        <TouchableWithoutFeedback onPress={onNotiPress}>
+          <Image
+            source={require("../../assets/icon_notification.png")}
+            style={{ height: 22, width: 22 }}
+            resizeMode="contain"
+          />
+        </TouchableWithoutFeedback>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-    container: {
-        height: 55,
-        backgroundColor: 'white',
-        justifyContent: 'space-between',
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: 10
-    }
-
-})
-export default Header
+  container: {
+    height: 55,
+    backgroundColor: "white",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+});
+export default Header;
--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import { StyleSheet, View, Text, TouchableOpacity, Image, Dimensions } from 'react-native'
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import TextInputComponent from './TextInputComponent';
 const screenWidth = Dimensions.get('window').width
 
-const SearchComponent = ({ onPress, movieSelection, onChangeText, value, searchStyle }) => {
+const SearchComponent = ({ onPress, movieSelection, onChangeText, value, searchStyle,onCrossPress }) => {
     return (
-        <TouchableOpacity onPress={onPress} style={[styles.container, searchStyle]}>
-            <EvilIcons size={26} color="gray" name="search" style={{ left: 10 }} />
-            <TextInputComponent
-                inputStyle={{ width: '90%', borderWidth: 0, width: screenWidth / 1.3, }}
-                placeholder={'search'}
-                onChangeText={onChangeText}
-                value={value}
-            />
-            {/* <AntDesign size={20} color="black" name="down" /> */}
-        </TouchableOpacity>
+        <View onPress={onPress} style={[styles.container, searchStyle]}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center',width:'70%',height:40 }}>
+                <EvilIcons size={26} color="gray" name="search" style={{}} />
+                <TextInputComponent
+                    inputStyle={{ borderWidth: 0, width: screenWidth /2, }}
+                    placeholder={'search'}
+                    onChangeText={onChangeText}
+                    value={value}
+                />
+            </View>
+            <Entypo onPress={onCrossPress} size={25} color="black" name="cross" />
+        </View>
     )
 }
 const styles = StyleSheet.create({
     container: {
-        width: "90%",
+        width: screenWidth/1.24,
         height: 40,
         elevation: 4,
-        alignSelf: 'center',
+        // alignSelf: 'center',
         backgroundColor: 'white',
         justifyContent: 'space-between',
         alignItems: 'center',
@@ -32,7 +34,8 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
         margin: 10,
         borderRadius: 10,
-        borderWidth: 1
+        borderWidth: 1,
+        // backgroundColor:'red'
 
     },
     textStyle: {

0171900303f41c2444aa3053e95d43662e0d9982
Saurabh Sindhu <saurabhsindhu96@gmail.com>
2022-11-25 09:25:13 -0800

    Components update with styling

--- src/components/ChildrenComponent.js
+++ src/components/ChildrenComponent.js
@@ -2,11 +2,27 @@ import React from "react";
 import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
 import Feather from "react-native-vector-icons/Feather";
 
-const ChildComponent = ({ onPress, name, isParent }) => {
+const ChildComponent = ({
+  onPress,
+  name,
+  isParent,
+  kidSelected,
+  parentSelected,
+}) => {
   return (
     <View style={{ width: 120, alignItems: "center" }}>
       <TouchableOpacity onPress={onPress} style={{}}>
-        <Image source={require('../../assets/Ellipse68.png')} style={{ height: 100,width:100, marginBottom:10}} />
+        <Image
+          source={require("../../assets/Ellipse68.png")}
+          style={{
+            height: 100,
+            width: 100,
+            marginBottom: 10,
+            borderWidth: ( parentSelected || kidSelected ? 3 :0 ),
+            borderColor: "#FFAB00",
+            borderRadius: 50,
+          }}
+        />
       </TouchableOpacity>
       <Text>{name}</Text>
     </View>

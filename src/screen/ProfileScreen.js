70b0c326d743df3a6115db59cf8632ba99ab4209
Saurabh Sindhu <saurabhsindhu96@gmail.com>
2022-11-25 00:51:59 -0800

    Profile Section Update

--- src/screen/ProfileScreen.js
+++ src/screen/ProfileScreen.js
@@ -1,69 +1,130 @@
 import React, { useState } from "react";
-import { StyleSheet,View,Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/Header";
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import AntDesign from "react-native-vector-icons/AntDesign";
 
-const ProfileScreen  = ({navigation})=>{
-    let data=[
-        {id:1,screen:'Profile Screen'},
-        {id:2,screen:'Manage Accounts'},
-        {id:3,screen:'Change Password'},
-        {id:4,screen:'Push Notification'}
-    ]
-    const [profileData,setProfileData] = useState(data)
-    const navigateToprofileScreen =(profile)=>{
-         if(profile==='Profile Screen') {
-            navigation.navigate('Profile')
-         } else if(profile==='Manage Accounts') {
-              navigation.navigate("ManageAccounts")
-         } else if(profile==='Change Password') {
-            navigation.navigate('ForgotPassword')
-         } else if(profile==='Push Notification') {
-            navigation.navigate('PushNotification')
-         }
+const ProfileScreen = ({ navigation }) => {
+  let data = [
+    { id: 1, screen: "Personal Setting" },
+    { id: 2, screen: "Manage Accounts" },
+    { id: 3, screen: "Change Password" },
+    { id: 4, screen: "Push Notification" },
+    { id: 5, screen: "Log out" },
+  ];
+  const [profileData, setProfileData] = useState(data);
+  const navigateToprofileScreen = (profile) => {
+    if (profile === "Personal Setting") {
+      navigation.navigate("Profile");
+    } else if (profile === "Manage Accounts") {
+      navigation.navigate("ManageAccounts");
+    } else if (profile === "Change Password") {
+      navigation.navigate("ForgotPassword");
+    } else if (profile === "Push Notification") {
+      navigation.navigate("PushNotification");
+    }else if (profile === "Log out") {
+      navigation.navigate("login")
     }
-    const renderItem = ({item})=>{
-         return (
-            <TouchableOpacity onPress={()=>navigateToprofileScreen(item.screen)} style={{margin:5}}>
-                <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',paddingHorizontal:13}}>
-                   <Text style={{margin:10}}>{item.screen}</Text>
-                   <AntDesign onPress={()=>navigateToprofileScreen(item.screen)}  size={20} color="black" name="right" />
-                </View>
-                <View style={{width:'90%',height:2,backgroundColor:'black',marginBottom:10,alignSelf:'center'}}/>
-            </TouchableOpacity>
-         )
-    }
+  };
+  const renderItem = ({ item }) => {
     return (
-      <SafeAreaView style={styles.container}>
-        <Header
-          onBackPress={() => navigation.pop()}
-          onNotiPress={() => navigation.navigate("PushNotification")}
-          onUserPress={() => navigation.navigate("ProfileScreen")}
-        />
-        <View style={{ flex: 1 }}>
-          <View
+      <TouchableOpacity
+        onPress={() => navigateToprofileScreen(item.screen)}
+        style={{ margin: 5 }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 13,
+            marginVertical: 8,
+          }}
+        >
+          <Text
             style={{
-              height: 30,
-              width: "100%",
-              justifyContent: "center",
-              margin: 20,
+              margin: 10,
+              fontSize: 16,
+              fontFamily: "Outfit-regular",
+              fontWeight: "400",
+              color: item.screen === "Log out" ?  "#FC6474": '#000'
             }}
           >
-            <Text style={{ fontSize: 20 }}>Profile</Text>
-          </View>
-          <FlatList
-            data={profileData}
-            keyExtractor={(item) => item.id}
-            renderItem={renderItem}
-          />
+            {item.screen}
+          </Text>
+          {item.screen !== "Log out" && (
+            <AntDesign
+              onPress={() => navigateToprofileScreen(item.screen)}
+              size={20}
+              color="#FFAB00"
+              name="right"
+            />
+          )}
         </View>
-      </SafeAreaView>
+
+        {item.screen !== "Log out" && (
+          <View
+            style={{
+              width: "90%",
+              height: 2,
+              backgroundColor: "black",
+              marginBottom: 10,
+              alignSelf: "center",
+            }}
+          />
+        )}
+      </TouchableOpacity>
     );
-}
-const styles= StyleSheet.create({
-    container:{
-        flex:1,
-    }
-})
-export default ProfileScreen
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header
+        logo
+        onLogoPress={() => navigation.pop()}
+        onNotiPress={() => navigation.navigate("PushNotification")}
+        onUserPress={() => navigation.navigate("ProfileScreen")}
+      />
+      <View style={{ flex: 1,            backgroundColor:'#fff'
+ }}>
+        <View
+          style={{
+            height: 30,
+            width: "100%",
+            justifyContent: "center",
+            marginTop: 20,
+            backgroundColor:'#fff'
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 24,
+              alignSelf: "center",
+              fontWeight: "400",
+              fontFamily: "Sniglet-regular",
+            }}
+          >
+            Profile
+          </Text>
+        </View>
+        <FlatList
+          data={profileData}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+export default ProfileScreen;

cf15abc96463247a306a8489beead3c1e3c99d58
Rahul Naranthatta <rnaranthatta00@mylangara.ca>
2022-12-02 11:34:23 -0800

    child component

--- src/screen/ChildScreen.js
+++ src/screen/ChildScreen.js
@@ -6,6 +6,7 @@ import {
   FlatList,
   Image,
   Dimensions,
+  TouchableWithoutFeedback,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import ApiHandler from "../api/ApiHandler";
@@ -14,10 +15,29 @@ import ChildComponent from "../components/ChildrenComponent";
 
 const ChildScreen = ({ navigation, route }) => {
   const [kidData, setKidData] = useState([]);
+  const [isParent, setIsParent] = useState(false);
+  const [kidName, setKidName] = useState("");
 
   useEffect(() => {
     getKid();
   }, []);
+
+  const navigateOnSelect = (isParent) => {
+    if (isParent) {
+      navigation.navigate("Pantry", {
+        kid: kidData,
+        parentId: route?.params?.parentId,
+      });
+    } else {
+      navigation.navigate("ChildTabNavigators", {
+        screen: "ChildLunchBox",
+        params: {
+          parentId: route.params.parentId,
+          name: kidName,
+        },
+      });
+    }
+  };
   const getKid = () => {
     ApiHandler.getKid(route.params.parentId).then((response) => {
       console.log("response===", response.kids);
@@ -65,59 +85,65 @@ const ChildScreen = ({ navigation, route }) => {
               justifyContent: "center",
               alignItems: "center",
               paddingTop: 40,
+              //   borderWidth: isParent ? 2 : 0,
+              // borderColor: "#FFAB00",
+              // borderRadius:100
             }}
           >
             <ChildComponent
               isParent={true}
+              parentSelected={isParent}
               name={route?.params?.userName}
               imgSource={require("../../assets/Ellipse67.png")}
-              onPress={() =>
-                navigation.navigate("Pantry", {
-                  kid: kidData,
-                  parentId: route?.params?.parentId,
-                })
-              }
+              onPress={() => {
+                setIsParent(true);
+              }}
             />
           </View>
           <View style={{ alignItems: "center", paddingTop: 60 }}>
             <FlatList
               data={kidData}
-              keyExtractor={(item) => item.id}
+              keyExtractor={(item, index) => item.id}
               numColumns={2}
-              renderItem={({ item }) => {
+              renderItem={({ item , index}) => {
                 return (
                   <ChildComponent
                     name={item.name}
                     isParent={false}
-                    onPress={() =>
-                      navigation.navigate("ChildTabNavigators", {
-                        screen: "ChildLunchBox",
-                        params: {
-                          parentId: route.params.parentId,
-                          name: item.name,
-                        },
-                      })
+                    firstKid={index === 0 ? true : false}
+                    kidSelected={
+                      !isParent && kidName === item.name ? true : false
                     }
+                    onPress={() => {
+                      setKidName(item.name);
+                      setIsParent(false);
+                    }}
                   />
                 );
               }}
             />
           </View>
-          <View
-            style={{
-              alignSelf: "center",
-              marginTop: 20,
-              backgroundColor: "#FFAB00",
-              width: 145,
-              alignItems: "center",
-              paddingVertical: 15,
-              borderRadius: 30,
+          <TouchableWithoutFeedback
+            onPress={() => {
+              navigateOnSelect(isParent);
             }}
           >
-            <Text style={{ fontSize: 18, color: "#FFF", fontWeight: "400" }}>
-              {"Continue"}
-            </Text>
-          </View>
+            <View
+              style={{
+                alignSelf: "center",
+                marginTop: 20,
+                backgroundColor: "#FFAB00",
+                width: 145,
+                alignItems: "center",
+                paddingVertical: 15,
+                borderRadius: 30,
+              }}
+            >
+              <Text style={{ fontSize: 18, color: "#FFF", fontWeight: "400" }}>
+                {"Continue"}
+              </Text>
+            </View>
+          </TouchableWithoutFeedback>
         </View>
       </View>
     </SafeAreaView>
